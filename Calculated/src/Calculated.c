0a64ed87dc53616a02a695fa31af52781e2292c9
vlivon <61311919+vlivon@users.noreply.github.com>
2020-06-08 22:52:01 +0300

    Update Calculated.c

--- Calculated/src/Calculated.c
+++ Calculated/src/Calculated.c
@@ -5,6 +5,23 @@
  Version     :
  Copyright   : no
  Description : Calculator working w/ files
+ 
+ In this version of the calculator, 
+ it became possible to perform scalar and vector calculations with reading from a file and writing to a file. 
+ You can choose between the console and file management "Choose between file and console [f/c]: ". 
+ Type "f" to choose to work with files or "c" to work in the console. 
+ In the console version, if you chose work with numbers, you should follow what appears on the screen: 
+ enter the first number, enter one of the following operations: [*]multiplication, [/]division, 
+ [+]addition, [-]subtraction, [^]exponentiation, [!]factorial, and then enter the second number (not for factorial). 
+ Also in console mode you can work with vectors: enter size of vectors, enter first vector, enter your operation (+, - or *) 
+ and enter second vector. 
+ 
+ If you work with files, then you shold follow next rules:
+ Information format must be like: - s 3 2 for operations on numbers or * v 3 4 5 5 -1 2 -7 for operations with vectors 
+ where at 1st place - operation type, then mode type(s - num. operations, v - operations with vectors), then:
+ a) If you choose number operations then enter 2 numbers, splitting space, or 1 number if you want counting factorial.
+ b) If you choose vector operations then enter coordinate system type(n-dimensional), 
+ and then needful quantity of numbers like that (x1 y1 z1 x2 y2 z2)
  ============================================================================
  */
 #include <stdio.h>
@@ -38,13 +55,13 @@ int main(int argc , char *argv[] )
                 fscanf(input, " %c", &choice);                                  // Scan vector or number
                 switch(choice)                                                  // Choose vector or number
                 {
-                    case 's':
+                    case 's':                                                   // Reading operation
                         switch(operation)
                         {
                             case '+':
-                                fscanf(input, " %f", &first);
+                                fscanf(input, " %f", &first);                   // Reading numbers
                                 fscanf(input, " %f", &second);
-                                fprintf(output, "%f + %f = %f", first, second, first+second);
+                                fprintf(output, "%f + %f = %f", first, second, first+second);   // Writing result to file
                                 break;
                             case '-':
                                 fscanf(input, " %f", &first);
@@ -71,26 +88,26 @@ int main(int argc , char *argv[] )
                             case '!':
                                 result = 1;
                                 fscanf(input," %f",&first);
-                                if (first >= 0) for (int i=first; i!=1; i--) result = result*i;   // calculate factorial from back
+                                if (first >= 0) for (int i=first; i!=1; i--) result = result*i;   // Calculate factorial from back
                                 else fprintf(output, "This number is below zero. Please, try again.");
                                 fprintf(output, "!%f = %f", first, result);
                                 break;
                         }
                         break;
                     case 'v':
-                        v1=malloc(size*sizeof(int));
+                        v1=malloc(size*sizeof(int));                                        // Memory allocation for variables
                         v2=malloc(size*sizeof(int));
-                        switch(operation)
+                        switch(operation)                                                  // Choose operation
                         {
-                            case '+':
-                                fscanf(input, " %i", &size);
-                                for(int i=0;i<size;i++)
-                                fscanf(input, " %f",&v1[i]);
-                                for(int i=0;i<size;i++)
-                                fscanf(input, " %f",&v2[i]);
+                            case '+':                                     
+                                fscanf(input, " %i", &size);                              //
+                                for(int i=0;i<size;i++)                                  //
+                                fscanf(input, " %f",&v1[i]);                            // Reading from file
+                                for(int i=0;i<size;i++)                                //
+                                fscanf(input, " %f",&v2[i]);                          //
 
 
-                                fprintf(output, "( ");
+                                fprintf(output, "( ");                                 
                                 for(int i=0;i<size;i++)
                                 fprintf(output, "%.0f ", v1[i]);
                                 fprintf(output, ") + ( ");
@@ -98,9 +115,9 @@ int main(int argc , char *argv[] )
                                 fprintf(output, "%.0f ", v2[i]);
                                 fprintf(output, ") = ( ");
                                 for(int i=0;i<size;i++)
-                                fprintf(output, "%.0f ",v1[i]+v2[i]);
+                                fprintf(output, "%.0f ",v1[i]+v2[i]);                // Writing result to file
                                 fprintf(output, ")");
-                                free(v1);
+                                free(v1);                                            // Freeing a memory cell
                                 free(v2);
                                 break;
                             case '-':
